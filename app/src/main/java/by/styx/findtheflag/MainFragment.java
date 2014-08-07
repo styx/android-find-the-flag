1d0073c30b8187fc75a05537d449dcedafa44425
Mikhail S. Pobolovets <styx.mp@gmail.com>
2014-08-07 19:43:37 +0300

    Initial commit
    Signed-off-by: Mikhail S. Pobolovets <styx.mp@gmail.com>

---
+++ app/src/main/java/by/styx/findtheflag/MainFragment.java
@@ -0,0 +1,23 @@
+package by.styx.findtheflag;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
